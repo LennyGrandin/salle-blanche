8258609a7debbc09fa0077ebd1d3593595df9243
SGLover28 <lilian.hennechart.ETU@univ-lemans.fr>
2021-02-10 21:59:12 +0100

    Gros upload de la mort, va-t-il fonctionner ?

---
+++ Assets/Script/doorScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorScript : MonoBehaviour
+{
+    private Animator anim;
+    public GameObject maPorte;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+     void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("OpenDoor");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        anim.enabled = true;
+        maPorte.GetComponent<RealSpace3D.RealSpace3D_AudioSource>().rs3d_PlaySound();
+        //sound.enabled = true;
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void pauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+}
